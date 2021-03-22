005d14e6091a4c5f271b63fe12a1d5429a226a5f
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-03-22 11:01:56 +0200

    update client library

---
+++ src/Service.Bitgo.WithdrawalProcessor.Client/AutofacHelper.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Service.Bitgo.WithdrawalProcessor.Grpc;
+// ReSharper disable UnusedMember.Global
+
+namespace Service.Bitgo.WithdrawalProcessor.Client
+{
+    public static class AutofacHelper
+    {
+        public static void RegisterBitgoCryptoWithdrawalClient(this ContainerBuilder builder, string balanceHistoryGrpcServiceUrl)
+        {
+            var factory = new BitgoWithdrawalProcessorClientFactory(balanceHistoryGrpcServiceUrl);
+
+            builder.RegisterInstance(factory.GetCryptoWithdrawalService()).As<ICryptoWithdrawalService>().SingleInstance();
+        }
+    }
+}
