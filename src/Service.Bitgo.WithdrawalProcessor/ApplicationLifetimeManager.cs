63949e8bd6536d119b1ea3e48a51ea29f5e17fa2
Dmitrii Krasovskikh <dmitry.krasovskih@gmail.com>
2021-05-24 12:11:19 +0300

    - start service bus client;

--- src/Service.Bitgo.WithdrawalProcessor/ApplicationLifetimeManager.cs
+++ src/Service.Bitgo.WithdrawalProcessor/ApplicationLifetimeManager.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using MyJetWallet.Sdk.Service;
 using MyNoSqlServer.DataReader;
+using MyServiceBus.TcpClient;
 
 namespace Service.Bitgo.WithdrawalProcessor
 {
@@ -9,12 +10,15 @@ namespace Service.Bitgo.WithdrawalProcessor
     {
         private readonly ILogger<ApplicationLifetimeManager> _logger;
         private readonly MyNoSqlTcpClient _myNoSqlTcpClient;
+        private readonly MyServiceBusTcpClient _myServiceBusTcpClient;
 
-        public ApplicationLifetimeManager(IHostApplicationLifetime appLifetime, ILogger<ApplicationLifetimeManager> logger, MyNoSqlTcpClient myNoSqlTcpClient)
+        public ApplicationLifetimeManager(IHostApplicationLifetime appLifetime, ILogger<ApplicationLifetimeManager> logger, MyNoSqlTcpClient myNoSqlTcpClient,
+            MyServiceBusTcpClient myServiceBusTcpClient)
             : base(appLifetime)
         {
             _logger = logger;
             _myNoSqlTcpClient = myNoSqlTcpClient;
+            _myServiceBusTcpClient = myServiceBusTcpClient;
         }
 
         protected override void OnStarted()
@@ -22,6 +26,8 @@ namespace Service.Bitgo.WithdrawalProcessor
             _logger.LogInformation("OnStarted has been called.");
             _myNoSqlTcpClient.Start();
             _logger.LogInformation("MyNoSqlTcpClient is started.");
+            _myServiceBusTcpClient.Start();
+            _logger.LogInformation("MyServiceBusTcpClient is started.");
         }
 
         protected override void OnStopping()
@@ -29,6 +35,8 @@ namespace Service.Bitgo.WithdrawalProcessor
             _logger.LogInformation("OnStopping has been called.");
             _myNoSqlTcpClient.Stop();
             _logger.LogInformation("MyNoSqlTcpClient is stopped.");
+            _myServiceBusTcpClient.Stop();
+            _logger.LogInformation("MyServiceBusTcpClient is stopped.");
         }
 
         protected override void OnStopped()
